09f093f262d4df0e8a6d15fa8e4bcaa9e27a6463
Jim Newpower <jimnewpower@gmail.com>
2023-04-30 11:45:21 -0600

    Fix build script

--- origin/build.sh
+++ origin/build.sh
@@ -2,10 +2,26 @@
 rm -rf bin/
 mkdir bin/
 
-cd src
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ../bin/main main.go
+cd src/origin
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ../../bin/origin origin.go
+chmod u+x ../../bin/origin
+cd ../..
 
-cd ../
+rm -rf archive
+mkdir archive
+cd archive
+cp ../bin/origin .
+cp ../cert/conjur-dev.pem .
+zip origin.zip origin conjur-dev.pem
+rm origin conjur-dev.pem
+less origin.zip
+cd ..
 
 echo "Contents of bin/"
 ls -l bin/
+
+echo "Contents of archive/"
+ls -l archive/
+
+echo "Contents of archive/origin.zip"
+unzip -l archive/origin.zip
