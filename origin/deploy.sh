303d3ccc3a90cb50c1337a177b566e8035012088
Jim Newpower <jimnewpower@gmail.com>
2023-05-01 15:04:51 -0600

    Copy in env.sh script to get variables into terraform

--- origin/deploy.sh
+++ origin/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#export TF_VAR_CONJUR_ACCOUNT="prima"
+#export TF_VAR_CONJUR_APPLIANCE_URL="https://ec2-34-204-42-151.compute-1.amazonaws.com"
+#export TF_VAR_CONJUR_CERT_FILE="./conjur-dev.pem"
+#export TF_VAR_CONJUR_AUTHN_LOGIN="admin"
+#export TF_VAR_CONJUR_AUTHN_API_KEY="18wv7sck9a66015fzsv3252qfvp23anzs81qkn4f916fbs3t228p4nb"
+#export TF_VAR_CONJUR_AUTHENTICATOR="authn-iam"
+
+(cp config/env/env.sh deploy/env.sh)
 (cd deploy; mkdir -p archive)
 (cd deploy; cp ../bin/main ./archive)
 (cd deploy; cp ../cert/conjur-dev.pem ./archive)
