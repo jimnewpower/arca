cc5002d53ec2b773a036ef3989e977ba600058d7
Jim Newpower <jimnewpower@gmail.com>
2023-05-01 15:04:11 -0600

    Change paths to policy files

--- origin/add_policy.sh
+++ origin/add_policy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-append_root='../bin/cybr conjur append-policy -b root -f ./policy/root-policy.yml'
+append_root='../bin/cybr conjur append-policy -b root -f ./config/policy/root-policy.yml'
 echo ${append_root}
 eval ${append_root}
 
-append_secrets='../bin/cybr conjur append-policy -b conjur/authn-iam/dev -f ./policy/secrets.yml'
-echo ${append_secrets}
-eval ${append_secrets}
+#append_secrets='../bin/cybr conjur append-policy -b conjur/authn-iam/dev -f ./config/policy/secrets.yml'
+#echo ${append_secrets}
+#eval ${append_secrets}
 
-append_application='../bin/cybr conjur append-policy -b originApplication -f ./policy/application.yml'
+append_application='../bin/cybr conjur append-policy -b originApplication -f ./config/policy/application.yml'
 echo ${append_application}
 eval ${append_application}
 
