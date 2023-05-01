e51cefa0973d49b3c23bc3f655695d81a8b1038b
Jim Newpower <jimnewpower@gmail.com>
2023-05-01 16:12:10 -0600

    Add echo lines

--- origin/check_policy.sh
+++ origin/check_policy.sh
@@ -2,14 +2,17 @@
 
 echo "Checking policy..."
 
+echo ""
 command='../bin/cybr conjur get-secret -i originApplication/connectionstring'
 echo ${command}
 eval ${command}
 
+echo ""
 command='../bin/cybr conjur get-secret -i originApplication/username'
 echo ${command}
 eval ${command}
 
+echo ""
 command='../bin/cybr conjur get-secret -i originApplication/password'
 echo ${command}
 eval ${command}
